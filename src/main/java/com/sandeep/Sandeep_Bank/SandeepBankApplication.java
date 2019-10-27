a539b34673e7c29793773c10da8cc7cee8d33ae6
sandeepthandrapadu <sandeepjava1987@gmail.com>
2019-10-26 22:44:55 -0500

    Bank application First commit

---
+++ src/main/java/com/sandeep/Sandeep_Bank/SandeepBankApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.Sandeep_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandeepBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandeepBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandeep/Sandeep_Bank/SandeepBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.Sandeep_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandeepBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
